ddfe161d74f4696f826c647662d711f375b63c14
Raynaldo Sutisna <raaynaldo@gmail.com>
2022-04-29 09:33:15 -0400

    feat: setup app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <AddTodo />
+      <Todos />
     </div>
   );
 }
